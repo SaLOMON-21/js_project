10919c11c91136670a204e422b741f2e0e2661af
SaLOMON <olawalesalomon21@gmail.com>
2023-08-21 19:20:01 +0000

    'ajout_de_fichier'

---
+++ code.js
@@ -0,0 +1,31 @@
+alert("ceci est une boite d'alerte1 :)");
+var visitorName=prompt("what's your name :") ;
+console.log(visitorName);
+var iscoolwithIt= confirm("are you ok whith it ?");
+console.log(iscoolwithIt);
+true;
+false;
+
+function checkifNumber(x){
+    if(typeof x==='number'){
+    console.log(x +'is a number.');
+    }
+}
+
+
+function decodeColor( code ) {
+    switch( code) {
+	case 1:
+		console.log( 'Red' );		
+	case 2:
+		console.log( 'Yellow' );		
+	case "x":
+		console.log( 'Green' );		
+	default:
+		console.log( 'Unknown code' );
+	}
+}
+
+
+
+c'est le lien de l'exercice svp .https://edabit.com/challenges/javascript
